80dc17eed4100c6d23ddff4d9e03fcd52a655f57
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2024-03-13 21:50:55 +0000

    The Micro frontend is configured and is running 🚀 ✅

--- frontend/auth/vite.config.ts
+++ frontend/auth/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "auth-app", // name of the fed group...
+      filename: "remoteEntry.js", // default file name
+      // Modules to expose
+      exposes: {
+        "./App": "./src/App.tsx",
+      },
+      shared: ["react", "react-dom"], // libs/deps to be shared
+    }),
+  ],
+  build: {
+    target: "esnext", // needed to final build
+  },
+});
--- frontend/finance/vite.config.ts
+++ frontend/finance/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "finance-app",
+      remotes: {
+        remoteApp: "http://localhost:5170/assets/remoteEntry.js", // Remove the trailing comma here
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+});
