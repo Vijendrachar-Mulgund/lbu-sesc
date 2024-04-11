b8f5056e1fbe600f3d7878958311ed6687d55dbf
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2024-04-11 22:25:19 +0100

    The Router and Tailwind is setup ✅

--- frontend/student/src/App.tsx
+++ frontend/student/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<div>This is the Student micro frontend</div>} />
-        <Route path="/home" element={<div>This is the home route</div>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<Signup />} />
       </Routes>
     </>
   );
