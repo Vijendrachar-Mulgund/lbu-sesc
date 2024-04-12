b8fe4d534a01f5a0bed446d29d8874b07e25ae5b
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2024-04-12 02:07:49 +0100

    The UI of Login and Sign up page is completed ✅

--- frontend/student/src/pages/Login/index.tsx
+++ frontend/student/src/pages/Login/index.tsx
@@ -1,16 +1,29 @@
 import { Input, Button } from "@nextui-org/react";
+import LeedsBeckettUniversity from "../../assets/lbu_logo.svg";
+import { useForm } from "react-hook-form";
+
+interface IFormSignInInput {
+  email: string;
+  password: string;
+}
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormSignInInput>();
+
+  const signUserIn = (data: IFormSignInInput) => {
+    console.log(data);
+  };
+
   return (
     <>
       <div className="flex min-h-full h-screen flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8">
         {/* Logo Section */}
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+          <img className="mx-auto h-24 w-auto" src={LeedsBeckettUniversity} alt="Leeds Beckett University Logo" />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
@@ -18,10 +31,26 @@ export default function Login() {
 
         {/* Form Section */}
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-            <Input type="email" label="Email" placeholder="Enter your email" />
-            <Input type="password" label="Password" placeholder="Enter password" />
-            <Button fullWidth color="primary">
+          <form className="space-y-6" onSubmit={handleSubmit(signUserIn)}>
+            <Input
+              type="email"
+              label="Email"
+              placeholder="Enter your email"
+              {...register("email", { required: true })}
+            />
+
+            {errors.email?.type === "required" && <p className="text-red-800 mx-5">Please enter your email address</p>}
+
+            <Input
+              type="password"
+              label="Password"
+              placeholder="Enter password"
+              {...register("password", { required: true })}
+            />
+
+            {errors.password?.type === "required" && <p className="text-red-800 mx-5">Please enter your password</p>}
+
+            <Button type="submit" fullWidth color="primary">
               Login
             </Button>
           </form>
--- frontend/student/src/pages/Register/index.tsx
+++ frontend/student/src/pages/Register/index.tsx
@@ -1,16 +1,32 @@
 import { Input, Button } from "@nextui-org/react";
+import { useForm } from "react-hook-form";
+import { emailRegex } from "../../utils";
+import LeedsBeckettUniversity from "../../assets/lbu_logo.svg";
+
+interface IRegisterFormInput {
+  email: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+}
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IRegisterFormInput>();
+
+  const registerNewAccount = (data: IRegisterFormInput) => {
+    console.log(data);
+  };
   return (
     <>
       <div className="flex min-h-full h-screen flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8">
         {/* Logo Section */}
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+          <img className="mx-auto h-24 w-auto" src={LeedsBeckettUniversity} alt="Leeds Beckett University Logo" />
+
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Register your new account
           </h2>
@@ -18,12 +34,43 @@ export default function Register() {
 
         {/* Form Section */}
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-            <Input type="email" label="Email" placeholder="Enter your email" />
-            <Input type="text" label="First name" placeholder="Enter your first name" />
-            <Input type="text" label="Last name" placeholder="Enter your last name" />
-            <Input type="password" label="Password" placeholder="Enter password" />
-            <Button fullWidth color="primary">
+          <form className="space-y-6" onSubmit={handleSubmit(registerNewAccount)}>
+            <Input
+              type="email"
+              label="Email"
+              placeholder="Enter your email"
+              {...register("email", { required: true, pattern: emailRegex })}
+            />
+            {errors.email?.type === "required" && <p className="text-red-800 mx-5">Please enter your email address</p>}
+            {errors.email?.type === "pattern" && (
+              <p className="text-red-800 mx-5">Please enter a valid Email address</p>
+            )}
+
+            <Input
+              type="text"
+              label="First name"
+              placeholder="Enter your first name"
+              {...register("firstname", { required: true })}
+            />
+            {errors.firstname?.type === "required" && <p className="text-red-800 mx-5">Please Enter your First name</p>}
+
+            <Input
+              type="text"
+              label="Last name"
+              placeholder="Enter your last name"
+              {...register("lastname", { required: true })}
+            />
+            {errors.lastname?.type === "required" && <p className="text-red-800 mx-5">Please Enter your First name</p>}
+
+            <Input
+              type="password"
+              label="Password"
+              placeholder="Enter password"
+              {...register("password", { required: true })}
+            />
+            {errors.password?.type === "required" && <p className="text-red-800 mx-5">Please Enter your Password</p>}
+
+            <Button type="submit" fullWidth color="primary">
               Register
             </Button>
           </form>
